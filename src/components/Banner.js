031a8a4afd73b8747aed37ff92ca1fa965847dc9
Cheri Hutchinson <cheribird@msn.com>
2023-08-14 17:00:33 -0500

    React buttons and table

---
+++ src/components/Banner.js
@@ -0,0 +1,10 @@
+function Banner() {
+    return (
+        <div>
+            <header>Orbit Report</header>
+            <p>Click the 'Orbit Type' buttons to see the satellites in that orbit type.</p>
+        </div>
+    );
+}
+
+export default Banner;
